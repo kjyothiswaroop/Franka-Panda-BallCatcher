0555e42a199828bce4c31486c3e595a8f9924137
kjyothiswaroop <kjyothiswaroop00@gmail.com>
2025-11-20 19:06:28 -0600

    Massive commit : Added 'aruco_detect' node for detection of aruco marker. Launch file added for the aruco node. Made multiple changes for OpenCV Version > 4.7. Added catchers_vision_interfaces. The full pipeline works to detect aruco. Colcon tests pass for this part.

--- catchers_vision/setup.py
+++ catchers_vision/setup.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from setuptools import find_packages, setup
 
+
 def recursive_files(prefix, path):
     """
     Recurse over path returning a list of tuples suitable for use with setuptools data_files.
@@ -42,6 +43,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'aruco_detect = catchers_vision.aruco:main'
         ],
     },
 )
